2af588846191983a4ed7692e99d9d8311d3e32c3
Kirill Topolyan <kirill@topolyan.com>
2016-10-05 19:31:23 +0300

    Update menudata.service.js

--- module-4-solution/src/menudata.service.js
+++ module-4-solution/src/menudata.service.js
@@ -1,8 +1,7 @@
 (function () {
 'use strict';
-angular.module('data')
+angular.module('data').constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
 .service('MenuDataService', MenuDataService);
-constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
  MenuDataService.$inject = ['$http', 'ApiBasePath'];
  function MenuDataService($http) {
   var service = this;
